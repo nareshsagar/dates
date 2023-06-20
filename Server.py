3484049fc28a4e376eb29a6c0687cdb5a174c5e6
nareshsagar <51433983+nareshsagar@users.noreply.github.com>
2023-06-20 14:34:38 +0530

    Update Server.py

--- Server.py
+++ Server.py
@@ -1,6 +1,6 @@
 import configparser
 
-def add_servers_to_serverclass(serverclass_conf_path, serverclass_name, new_server_hosts):
+def add_servers_to_serverclass(serverclass_conf_path, serverclass_name, new_server_ips):
     config = configparser.ConfigParser()
     config.read(serverclass_conf_path)
 
@@ -8,28 +8,58 @@ def add_servers_to_serverclass(serverclass_conf_path, serverclass_name, new_serv
         print(f"{serverclass_name} class not found in serverclass.conf.")
         return
 
-    existing_hosts = set(config.get(serverclass_name, 'whitelist', fallback='').split(','))
+    existing_servers = set()
+    for key, value in config.items(serverclass_name):
+        if key.startswith("whitelist"):
+            existing_servers.add(value)
 
-    added_hosts = []
+    added_servers = []
 
-    for new_server_host in [host.strip() for host in new_server_hosts.split(',')]:
-        if new_server_host in existing_hosts:
-            print(f"Host {new_server_host} is already mapped to {serverclass_name} class.")
-        else:
-            existing_whitelist = config.get(serverclass_name, 'whitelist', fallback='')
-            updated_whitelist = f"{existing_whitelist},{new_server_host}" if existing_whitelist else new_server_host
-            config.set(serverclass_name, 'whitelist', updated_whitelist)
-            added_hosts.append(new_server_host)
+    for new_server_ip in [ip.strip() for ip in new_server_ips.split(',')]:
+        if new_server_ip in existing_servers:
+            print(f"Server {new_server_ip} is already mapped to {serverclass_name} class.")
+        else: ki
+            whitelist_key = f"whitelist.{len(existing_servers) + 1}"
+            config.set(serverclass_name, whitelist_key, new_server_ip)
+            added_servers.append(new_server_ip)
 
     with open(serverclass_conf_path, 'w') as configfile:
         config.write(configfile)
 
-    if added_hosts:
-        print(f"Successfully added {', '.join(added_hosts)} to {serverclass_name} class.")
+    if added_servers:
+        print(f"Successfully added {', '.join(added_servers)} to {serverclass_name} class.")
 
-# Example usage
-serverclass_conf_path = 'serverclass.conf'
-serverclass_name = 'role_power'
-new_server_hosts = 'host1, host2, host3'
 
-add_servers_to_serverclass(serverclass_conf_path, serverclass_name, new_server_hosts)
+add_servers_to_serverclass("serverclass.conf", "my_serverclass", "host3, host4")
+
+
+import configparser
+
+def remove_servers_from_serverclass(serverclass_conf_path, serverclass_name, server_ips_to_remove):
+    config = configparser.ConfigParser()
+    config.read(serverclass_conf_path)
+
+    if serverclass_name not in config.sections():
+        print(f"{serverclass_name} class not found in serverclass.conf.")
+        return
+
+    existing_servers = config.get(serverclass_name, 'whitelist', fallback='').split(',')
+
+    removed_servers = []
+
+    for server_ip_to_remove in [ip.strip() for ip in server_ips_to_remove.split(',')]:
+        if server_ip_to_remove in existing_servers:
+            existing_servers.remove(server_ip_to_remove)
+            removed_servers.append(server_ip_to_remove)
+        else:
+            print(f"Server {server_ip_to_remove} is not found in {serverclass_name} class.")
+
+    config.set(serverclass_name, 'whitelist', ','.join(existing_servers))
+
+    with open(serverclass_conf_path, 'w') as configfile:
+        config.write(configfile)
+
+    if removed_servers:
+        print(f"Successfully removed {', '.join(removed_servers)} from {serverclass_name} class.")
+
+
